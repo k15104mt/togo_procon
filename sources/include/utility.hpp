cb4cf2efcd39c05b04faff95393f23ae8d988b4c
Kentarou Ueda <ueken0307@hotmail.co.jp>
2017-05-07 01:15:45 +0900

    関数の宣言のほうの引数名を消して見やすくした（当社比5倍）

--- sources/include/utility.hpp
+++ sources/include/utility.hpp
@@ -8,6 +8,7 @@ struct Point{
 
 //二つの線分の始点、終点から、線分同士が交差しているかを返す
 //線分が平行な場合は交差していないものとなる
-int crossLine(Point Line1Start,Point Line1End,Point Line2Start,Point Line2End);
+int crossLine(Point,Point,Point,Point);
+
 
 #endif

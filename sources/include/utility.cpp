1b55c311ea2db0363481df0707a9c8c8d8166e55
Kentarou Ueda <ueken0307@hotmail.co.jp>
2017-05-09 18:26:52 +0900

    なんかあたり判定書けた（確認したとは言ってない)

--- sources/include/utility.cpp
+++ sources/include/utility.cpp
@@ -3,7 +3,7 @@
 
 //二つの線分の始点、終点から、線分同士が交差しているかを返す
 //線分が平行な場合は交差していないものとなる
-int crossLine(Point Line1Start,Point Line1End,
+int cl(Point Line1Start,Point Line1End,
               Point Line2Start,Point Line2End){
     
   if((((Line1Start.x-Line1End.x)*(Line2Start.y-Line1Start.y)+
@@ -18,4 +18,42 @@ int crossLine(Point Line1Start,Point Line1End,
 		}
     
 	return 0;
+}
+
+int crossLine(std::vector<Point> data1,std::vector<Point> data2){
+  for(int i=0;i<data1.size();++i){
+    for(int j=0;j<data2.size();++j){
+      if(cl(data1[i],data1[(i+1)%static_cast<int>(data1.size())],
+                   data2[j],data2[(j+1)%static_cast<int>(data2.size())])){
+        return 1;
+      }
+    }
+  }
+	return 0;
+}
+
+int inPolygon(std::vector<Point> data1,std::vector<Point> data2){
+  //data2のi番目の点がdata1のポリゴン内にあるかどうか
+  for(int i=0;i<data2.size();++i){
+    double x = 0.0;
+    for(int j=0;j<data1.size();++j){
+      Vector a = data1[(j+1)%data1.size()]-data2[i];
+      Vector b = data1[j]-data2[i];
+      
+      if(dot(a,b) <= 0 && cross(a,b) == 0){
+        break;
+      }
+      
+      //角度を積極的に足していこう
+      if(cross(a,b)<0) x+=(acos((double)(dot(a,b))/(a.size()*b.size()))*180.0/acos(-1.0));
+  		if(cross(a,b)>0) x+=-1.0*(acos((double)(dot(a,b))/(a.size()*b.size()))*180.0/acos(-1.0));	
+    }
+    
+    if(i!=data2.size() || x==360){
+      
+    }else{
+      return 0;
+    }
+  }
+  return 1;
 }
--- sources/include/utility.hpp
+++ sources/include/utility.hpp
@@ -1,14 +1,34 @@
 #ifndef _UTILITY_HPP_
 #define _UTILITY_HPP_
 
-struct Point{
-  int x;
-  int y;
+#include<vector>
+#include<cmath>
+
+class Point{
+public:
+	int x,y;
+	Point(int x,int y):x(x),y(y){};
+	Point & operator + (Point &obj){return Point(x+obj.x,y+obj.y);};
+	Point & operator - (Point &obj){return Point(x-obj.x,y-obj.y);};
+  Point & operator = (Point &obj){x = obj.x; y = obj.y; return *this;};
+  bool operator == (Point &obj){return x==obj.x && y==obj.y;};
+  double size(){return sqrt(x*x+y*y);}
 };
 
-//二つの線分の始点、終点から、線分同士が交差しているかを返す
-//線分が平行な場合は交差していないものとなる
-int crossLine(Point,Point,Point,Point);
+//ベクトルも同じ型みたいなものだし多少はね
+using vector = Point;
 
+//内積ィ！！
+int dot(Vector a,Vector b){return a.x * b.x + a.y * b.y;}
+//外積ィ！！
+int cross(Vector a,Vector b){return a.x * b.x - a.y * b.x;}
+
+//すべての辺を見て交わっているものがあった場合は１を返す
+int crossLine(std::vector<Point>,std::vector<Point>);
+
+//第一引数のポリゴンに第二引数のすべての頂点が内包されている場合または
+//第二引数のポリゴンに第一引数のすべての頂点が内包されている場合に1を返す
+//返上にある場合も内側に入っているものとする
+int inPolygon(std::vector<Point>,std::vector<Point>);
 
 #endif

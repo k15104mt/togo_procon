eb2f98468d7c119b9eb6f42fe9d618f69cec1dd3
Kentarou Ueda <ueken0307@hotmail.co.jp>
2017-08-23 14:30:55 +0900

    pieceコンストラクタの改善

--- sources/include/piece.cpp
+++ sources/include/piece.cpp
@@ -1,4 +1,5 @@
 #include"piece.hpp"
+#include<algorithm>
 #include<cstdlib>	//// abs() for integer
 #define PI 3.14159265358979
 
@@ -24,31 +25,19 @@ Point MinPoint(std::vector<Point> &data){
 Piece::Piece(std::vector<Point> &data){
   int num = data.size();     				//頂点数
   std::vector<Point> tmp(num); 			//1回転パターンの一時格納配列
-	Point min;min.x=0,min.y=0;				//ずらすために取得する座標最小値
-
-	for (int i = 0; i<4; i++){  			//4回回転(90度ずつ)
-    //printf("--回転パターン[%dπ/2]--\n", i); //debug
-    for (int j = 0; j < num; j++) {
-      tmp[j] = PointRotate((1.0 / 2.0)*i*PI, data[j]);
-			//printf("前%d,%d\n",tmp[j].x, tmp[j].y); //debug
-			if(min.x>tmp[j].x)min.x=tmp[j].x;
-			if(min.y>tmp[j].y)min.y=tmp[j].y;
-    }
-
-		if(min.x<0){
-			for(int j=0;j<num;j++){
-				tmp[j].x+=std::abs(min.x);
-				//printf("後%d,%d\n",tmp[j].x, tmp[j].y); //debug
-			}
-		}
-		if(min.y<0){
-			for(int j=0;j<num;j++){
-				tmp[j].y+=std::abs(min.y);
-				//printf("後%d,%d\n",tmp[j].x, tmp[j].y); //debug
-			}
-		}
-    point.push_back(tmp); //一つの回転パターンをpushback
-		min.x=0; min.y=0;
+  
+  for (int i = 0; i<4; i++){  			//4回回転(90度ずつ)
+	Point min;	//ずらすために取得する座標最小値
+	//printf("--回転パターン[%dπ/2]--\n", i); //debug
+	for (int j = 0; j < num; j++) {
+	  tmp[j] = PointRotate((1.0 / 2.0)*i*PI, data[j]);
+	  if (j == 0) { min.x = tmp[j].x; min.y = tmp[j].y; }
+	  //printf("前%d,%d\n",tmp[j].x, tmp[j].y); //debug
+	  min.x = std::min(min.x, tmp[j].x);
+	  min.y = std::min(min.y, tmp[j].y);
+	}
+	move(tmp, Point(-min.x,-min.y));
+	point.push_back(tmp); //一つの回転パターンをpushback
   }
 }
 

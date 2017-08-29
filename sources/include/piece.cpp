f4aeab6ccd3eaa172291b5fb6ecddb583d2b3e5b
k15105mt <k15105mt@apps.kct.ac.jp>
2017-08-29 13:54:24 +0900

    ピースの反転パターンの格納を追加

--- sources/include/piece.cpp
+++ sources/include/piece.cpp
@@ -11,6 +11,19 @@ Point PointRotate(double angle, Point zeroPoint) {
 	return r;
 }
 
+//頂点の順を逆に(反時計回り→時計回り)
+void reverse(std::vector<Point> &data) {
+	int	i, tmpx, tmpy,n=data.size();
+	for (i = 0; i < n/ 2; ++i) {
+		tmpx = data[i].x;
+		tmpy = data[i].y;
+		data[i].x = data[n - i - 1].x;
+		data[i].y = data[n - i - 1].y;
+		data[n - i - 1].x = tmpx;
+		data[n - i - 1].y = tmpy;
+	}
+}
+
 Piece::Piece(std::vector<Point> &data){
   int num = data.size();     				//頂点数
   std::vector<Point> tmp(num); 			//1回転パターンの一時格納配列
@@ -21,11 +34,24 @@ Piece::Piece(std::vector<Point> &data){
 	for (int j = 0; j < num; j++) {
 	  tmp[j] = PointRotate((1.0 / 2.0)*i*PI, data[j]);
 	  if (j == 0) { min.x = tmp[j].x; min.y = tmp[j].y; }
-	  //printf("前%d,%d\n",tmp[j].x, tmp[j].y); //debug
 	  min.x = std::min(min.x, tmp[j].x);
 	  min.y = std::min(min.y, tmp[j].y);
+	  //printf("(%d,%d)\n", tmp[j].x, tmp[j].y); //debug
 	}
 	move(tmp, Point(-min.x,-min.y));
+	point.push_back(tmp); //一つの回転パターンをpushback
+
+	//printf("--反転パターン[%dπ/2]--\n", i);
+	for (int j = 0; j < num; j++) {
+		tmp[j] = tmp[j] = PointRotate((1.0 / 2.0)*i*PI, data[j]);
+		tmp[j].x *= -1;
+		if (j == 0) { min.x = tmp[j].x; min.y = tmp[j].y; }
+		min.x = std::min(min.x, tmp[j].x);
+		min.y = std::min(min.y, tmp[j].y);
+		//printf("(%d,%d)\n", tmp[j].x, tmp[j].y); //debug
+	}
+	move(tmp, Point(-min.x, -min.y));
+	reverse(tmp);
 	point.push_back(tmp); //一つの回転パターンをpushback
   }
 }
